32ca5596bf88f95692e012d0bc79dec2ce2ef548
besta <linus.fulton@yahoo.com>
2020-11-01 17:36:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def ask():
+    name = input("What is your name?")
+    age = input("How old are you?")
+    username = input("What is your username?")
+
+    print("Your name is ", name, ", your age is ", age, ", and your username is ", username)
+
+
+if __name__ == '__main__':
+    ask()
